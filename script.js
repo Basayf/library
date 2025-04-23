23151e06200e5f7f9fc06a53aedf869e606fcf1a
Basayf <r.a.basayf@gmail.com>
2025-04-24 00:47:05 +0300

    Rewritten everything but styling

--- script.js
+++ script.js
@@ -1,51 +1,93 @@
-const myLibrary = [];
+const odinsLibrary = [];
+console.log(odinsLibrary);
 
-function Book (title, author, pages, status) {
+const addBookButton = document.getElementById('addBookButton');
+const dialog = document.getElementById('dialog'); 
+const submitButton = document.createElement('button');
+submitButton.textContent = 'Submit';
+const container = document.getElementById('container');
+
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.status = status;
 }
 
-function addBookToLibrary (title, author, pages, status) {
-
-    let aBook = new Book (title, author, pages, status);
-    myLibrary.push(aBook);
+function addBooksToLibrary(title, author, pages) {
+    const aBook = new Book (title, author, pages);
+    odinsLibrary.push(aBook);
 }
 
+addBookButton.addEventListener('click', () => {
+    dialog.showModal();
+    dialog.appendChild(submitButton);
+});
 
+submitButton.addEventListener('click', (event) => {
+    event.preventDefault();
+    const title = document.getElementById('bookTitle').value;
+    const author = document.getElementById('bookAuthor').value;
+    const pages = document.getElementById('bookPages').value;
+    console.log(bookTitle);
+    console.log(bookAuthor);
+    console.log(bookPages);
+    addBooksToLibrary(title, author, pages);
+    updateAndShowInfo();
+    dialog.close();
+});
 
-addBookToLibrary ("narnia", "jfjf", 878, "read");
-addBookToLibrary ("harry potter", "jfjf", 878, "read");
-addBookToLibrary ("new age", "jfjf", 878, "read");
-myLibrary.forEach((element) => {element.id = crypto.randomUUID()});
+function updateAndShowInfo() {
+    container.replaceChildren();
 
-const container = document.getElementById("container");
+    odinsLibrary.forEach((book) => {
+        const card = document.createElement('div');
+        const title = document.createElement('div');
+        const author = document.createElement('div');
+        const pages = document.createElement('div');
+        const ISBN = document.createElement('div');
+        const removeButton = document.createElement('button');
+        removeButton.textContent = 'Remove Book';
+        const status = document.createElement('button');
+        status.textContent = 'Read';
+        const statusOfReadingBox = document.createElement('div');
+        statusOfReadingBox.textContent = 'Not Read';
 
-for (let i = 0; i < myLibrary.length; i++) {
-    const card = document.createElement('div');
-    const title = document.createElement('div');
-    const author = document.createElement('div');
-    const pages = document.createElement('div');
-    const status = document.createElement('div');
-    const id = document.createElement('div');
-    title.className = "title";
-    card.className = "card";
-    author.className = "author";
-    pages.className = "pages";
-    status.className = "status";
-    id.className = "id";
-    title.innerText = `${myLibrary[i].title}`;
-    author.innerText = `${myLibrary[i].author}`;
-    pages.innerText = `${myLibrary[i].pages}`;
-    status.innerText = `${myLibrary[i].status}`;
-    id.innerText = `${myLibrary[i].id}`;
-    container.appendChild(card);
-    card.appendChild(title);
-    card.appendChild(author);
-    card.appendChild(pages);
-    card.appendChild(status);
-    card.appendChild(id);
- }
+        title.textContent = book.title;
+        author.textContent = book.author;
+        pages.textContent = book.pages;
+        ISBN.textContent = crypto.randomUUID();
 
-console.log(myLibrary);
+        container.appendChild(card);
+        card.appendChild(title);
+        card.appendChild(author);
+        card.appendChild(pages);
+        card.appendChild(ISBN);
+        card.appendChild(status);
+        card.appendChild(statusOfReadingBox);
+        card.appendChild(removeButton);
+
+        removeButton.addEventListener('click', () => {
+            removeBook(card.index);
+            updateAndShowInfo()
+        });
+
+        status.addEventListener('click', () => {
+            if (status.textContent === 'Read') {
+                statusOfReadingBox.textContent = 'Read';
+                status.textContent = 'Not Read';
+            } else {
+                statusOfReadingBox.textContent = 'Not Read';
+                status.textContent = 'Read';
+            }
+        });
+    });
+
+}
+
+function removeBook(index) {
+    const bookIndex = odinsLibrary.indexOf(index);
+    odinsLibrary.splice(bookIndex, 1);
+    console.log('index of removed book' + bookIndex);
+}
+
+ 
